f97fe1fdafc37ff78e6ed5449ca3e0e023c89a16
Sanyam Rawat <126306140+Yamyamlol@users.noreply.github.com>
2024-08-27 14:22:16 +0530

    Create Secant Method.c

---
+++ Secant Method.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <math.h>
+
+void details()
+{
+    printf("Name: Sanyam Rawat\nSection: D(G2)\nRno: 58\n\nSecant Method\n\n");
+}
+
+float f(float x)
+{
+    float eq = (x * x * x) - (5 * x) + 1;
+    return eq;
+}
+
+void main()
+{
+    int i = 0, n, flag;
+    float a, b, e;
+    float c = (a * f(b) - b * f(a)) / f(b) - f(a);
+
+    details();
+
+    printf("Enter the value of guesses (a and b) : ");
+    scanf("%f %f", &a, &b);
+    printf("Enter error : ");
+    scanf("%f", &e);
+    printf("Enter number of iterations : ");
+    scanf("%d", &n);
+
+    printf("\nA\t\tB\t\tC\n\n");
+    do
+    {
+        i++;
+        c = (a * f(b) - b * f(a)) / (f(b) - f(a));
+
+        printf("%f\t%f\t%f\n", a, b, c);
+
+        a = b;
+        b = c;
+
+    } while (fabs(f(c)) > e && i < n);
+    if (fabs(f(c)) <= e)
+    {
+        printf("\nFinal approximate root after %d iterations is : %f\n", i, c);
+    }
+    else
+    {
+        printf("\nMaximum iterations reached, approximate root not converged");
+    }
+}
